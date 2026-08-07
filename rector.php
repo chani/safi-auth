e03af11af57765aabcab3a5d8be0987f951471ff
Jean Bruenn <himself@jeanbruenn.info>
2026-08-07 22:15:58 +0200

    build: update rector paths

--- rector.php
+++ rector.php
@@ -5,10 +5,22 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\ValueObject\PhpVersion;
 
+$paths = [__DIR__ . '/src'];
+if (is_dir(__DIR__ . '/tests')) {
+    $paths[] = __DIR__ . '/tests';
+}
+if (is_dir(__DIR__ . '/components')) {
+    $paths[] = __DIR__ . '/components';
+}
+
 return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+    ->withPaths($paths)
+    ->withSkip([
+        '*/vendor/*',
+        '*/data/*',
+        '*/cache/*',
+        '*/var/*',
+        '*/storage/*',
     ])
     ->withPhpVersion(PhpVersion::PHP_85)
     ->withPreparedSets(
