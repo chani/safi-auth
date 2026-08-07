ab6c40e05c25a0aa894ba5489281fbf476f3aaf6
Jean Bruenn <himself@jeanbruenn.info>
2026-08-07 22:18:01 +0200

    refactor(auth): inject database initializer via constructor

--- src/Cli/Commands/AuthInitCommand.php
+++ src/Cli/Commands/AuthInitCommand.php
@@ -1,23 +1,15 @@
 <?php
 
-/**
- * Safi Microframework - safi-auth
- * @author Jean Bruenn
- * @copyright 2026 All Rights Reserved
- * @see https://github.com/chani/safi-auth
- */
-
 declare(strict_types=1);
 
 namespace Safi\Extensions\Auth\Cli\Commands;
 
 use Safi\Core\Cli\CommandInterface;
-use Safi\Core\Contracts\DatabaseDriverInterface;
 use Safi\Extensions\Auth\AuthDatabaseInit;
 
 final readonly class AuthInitCommand implements CommandInterface
 {
-    public function __construct(private DatabaseDriverInterface $db) {}
+    public function __construct(private AuthDatabaseInit $init) {}
 
     #[\Override]
     public function getName(): string
@@ -40,9 +32,7 @@ final readonly class AuthInitCommand implements CommandInterface
     #[\Override]
     public function execute(array $args): int
     {
-        $init = new AuthDatabaseInit($this->db);
-        $init->initializeSchema();
-        echo "Database schema initialized.\n";
+        $this->init->initializeSchema();
         return 0;
     }
 }
