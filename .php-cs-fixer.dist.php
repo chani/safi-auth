4e07c8a43dd20e94d14441de148509fea2929218
Jean Bruenn <himself@jeanbruenn.info>
2026-08-07 22:15:05 +0200

    build: update php-cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,29 +2,43 @@
 
 declare(strict_types=1);
 
-$finder = (new PhpCsFixer\Finder())
-    ->in([
-        __DIR__ . '/src',
-    ])
-    ->append([
-        __FILE__,
-    ]);
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = (new Finder())
+    ->in([__DIR__ . '/src'])
+    ->exclude(['vendor', 'data', 'cache', 'var', 'storage'])
+    ->notName('*.cache')
+    ->append([__FILE__]);
 
 if (is_dir(__DIR__ . '/tests')) {
     $finder->in(__DIR__ . '/tests');
 }
+if (is_dir(__DIR__ . '/components')) {
+    $finder->in(__DIR__ . '/components');
+}
 
-return (new PhpCsFixer\Config())
+return (new Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PER-CS2.0' => true,
+        '@PER-CS2.0:risky' => true,
         'declare_strict_types' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['class', 'function', 'const'],
+        ],
         'no_unused_imports' => true,
         'single_line_after_imports' => true,
         'trailing_comma_in_multiline' => [
-            'elements' => ['arrays', 'arguments', 'parameters'],
+            'elements' => ['arrays', 'arguments', 'parameters', 'match'],
+        ],
+        'nullable_type_declaration_for_default_null_value' => true,
+        'modernize_types_casting' => true,
+        'no_superfluous_phpdoc_tags' => [
+            'remove_inheritdoc' => true,
+            'allow_mixed' => true,
         ],
     ])
     ->setFinder($finder);
