08296e8860ab66b288bd50f47104e5db0fb7f363
Jean Bruenn <himself@jeanbruenn.info>
2026-08-09 14:03:10 +0200

    refactor(models): simplify property hook getters using expression hooks

--- src/Models/User.php
+++ src/Models/User.php
@@ -7,70 +7,49 @@ namespace Safi\Extensions\Auth\Models;
 final class User extends AbstractAuthModel
 {
     public string $email {
-        get {
-            $val = $this->getProperty('email', '');
-            return is_string($val) ? $val : '';
-        }
+        get => $this->getString('email');
         set {
             $this->setProperty('email', strtolower(trim($value)));
         }
     }
 
     public string $password {
-        get {
-            $val = $this->getProperty('password', '');
-            return is_string($val) ? $val : '';
-        }
+        get => $this->getString('password');
         set {
             $this->setProperty('password', $value);
         }
     }
 
     public string $role {
-        get {
-            $val = $this->getProperty('role', 'user');
-            return is_string($val) ? $val : 'user';
-        }
+        get => $this->getString('role', 'user');
         set {
             $this->setProperty('role', $value);
         }
     }
 
     public bool $mustChangePassword {
-        get {
-            $val = $this->getProperty('must_change_password', false);
-            return (bool) $val;
-        }
+        get => $this->getBool('must_change_password');
         set {
             $this->setProperty('must_change_password', $value ? 1 : 0);
         }
     }
 
     public string $totpSecret {
-        get {
-            $val = $this->getProperty('totp_secret', '');
-            return is_string($val) ? $val : '';
-        }
+        get => $this->getString('totp_secret');
         set {
             $this->setProperty('totp_secret', trim($value));
         }
     }
 
     public bool $is2faEnabled {
-        get {
-            $val = $this->getProperty('is_2fa_enabled', false);
-            return (bool) $val;
-        }
+        get => $this->getBool('is_2fa_enabled');
         set {
             $this->setProperty('is_2fa_enabled', $value ? 1 : 0);
         }
     }
 
     public string $createdAt {
-        get {
-            $val = $this->getProperty('created_at', '');
-            return is_string($val) ? $val : '';
-        }
+        get => $this->getString('created_at');
         set {
             $this->setProperty('created_at', $value);
         }
